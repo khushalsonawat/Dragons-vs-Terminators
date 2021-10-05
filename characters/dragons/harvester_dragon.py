3b8a1fecac6ae19542e03c6637456c84dadb3c51
ksonawat007 <ssks81285@gmail.com>
2021-10-05 18:51:15 +0530

    Implemented til phase 1.2

--- characters/dragons/harvester_dragon.py
+++ characters/dragons/harvester_dragon.py
@@ -6,13 +6,18 @@ class HarvesterDragon(Dragon):
 
     name = 'Harvester'
     implemented = True
-
+    
     # OVERRIDE CLASS ATTRIBUTES HERE
+    food_cost = 2
+    def __init__(self, armor=1):
+        super().__init__(armor = armor)
+        
 
     def action(self, colony):
         """Produce 1 additional food for the colony.
 
         colony -- The DragonColony, used to access game state information.
         """
+        colony.food += 1
         # BEGIN 1.1
         "*** YOUR CODE HERE ***"
